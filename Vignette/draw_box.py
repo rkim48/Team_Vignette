f7eff9d369d04c1e8913548c0693b7086120646e
cjhenders <cjhenders@users.noreply.github.com>
2019-04-30 23:07:23 -0500

    Add files via upload

---
+++ Vignette/draw_box.py
@@ -0,0 +1,154 @@
+import tensorflow as tf
+import os
+import numpy as np
+import cv2
+import warnings
+import sys
+import time
+warnings.filterwarnings('ignore')
+
+
+def sigmoid(x):
+  return 1. / (1. + np.exp(-x))
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    out = e_x / e_x.sum()
+    return out
+
+
+def iou(boxA,boxB):
+
+
+  # Determine the coordinates of the intersection rectangle
+  xA = max(boxA[0], boxB[0])
+  yA = max(boxA[1], boxB[1])
+  xB = min(boxA[2], boxB[2])
+  yB = min(boxA[3], boxB[3])
+ 
+  # Compute the area of intersection
+  intersection_area = (xB - xA + 1) * (yB - yA + 1)
+ 
+  # Compute the area of both rectangles
+  boxA_area = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+  boxB_area = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+  # Compute the IOU
+  iou = intersection_area / float(boxA_area + boxB_area - intersection_area)
+
+  return iou
+
+
+
+def non_maximal_suppression(thresholded_predictions,iou_threshold):
+
+  nms_predictions = []
+  nms_predictions.append(thresholded_predictions[0])
+  # thresholded_predictions[0] = [x1,y1,x2,y2]
+
+  i = 1
+  while i < len(thresholded_predictions):
+    n_boxes_to_check = len(nms_predictions)
+    #print('N boxes to check = {}'.format(n_boxes_to_check))
+    to_delete = False
+
+    j = 0
+    while j < n_boxes_to_check:
+        curr_iou = iou(thresholded_predictions[i][0],nms_predictions[j][0])
+        if(curr_iou > iou_threshold ):
+            to_delete = True
+        #print('Checking box {} vs {}: IOU = {} , To delete = {}'.format(thresholded_predictions[i][0],nms_predictions[j][0],curr_iou,to_delete))
+        j = j+1
+
+    if to_delete == False:
+        nms_predictions.append(thresholded_predictions[i])
+    i = i+1
+
+  return nms_predictions
+
+
+
+def postprocessing(predictions,input_img,score_threshold,iou_threshold,input_height,input_width):
+
+  input_image = input_img
+
+  n_classes = 3
+  n_grid_cells = 13
+  n_b_boxes = 5
+  n_b_box_coord = 4
+
+  # Names and colors for each class
+  classes = ["bounce","febreeze","gain","tide"]
+  colors = [(0, 254.0, 0),(254.0, 0, 0),(0, 0, 254.0),(0, 254.0, 254.0)]
+
+  # Pre-computed YOLOv2 shapes of the k=5 B-Boxes
+  anchors = [1.08,1.19,  3.42,4.41,  6.63,11.38,  9.42,5.11,  16.62,10.52]
+
+  thresholded_predictions = []
+  #print('Thresholding on (Objectness score)*(Best class score) with threshold = {}'.format(score_threshold))
+
+  # IMPORTANT: reshape to have shape = [ 13 x 13 x (5 B-Boxes) x (4 Coords + 1 Obj score + 20 Class scores ) ]
+  predictions = np.reshape(predictions,(13,13,5,9))
+
+  # IMPORTANT: Compute the coordinates and score of the B-Boxes by considering the parametrization of YOLOv2
+  for row in range(n_grid_cells):
+    for col in range(n_grid_cells):
+      for b in range(2,3):
+##      b=4
+##      if b == 3:
+        tx, ty, tw, th, tc = predictions[row, col, b, :5]
+
+        # IMPORTANT: (416 img size) / (13 grid cells) = 32!
+        # YOLOv2 predicts parametrized coordinates that must be converted to full size
+        # final_coordinates = parametrized_coordinates * 32.0 ( You can see other EQUIVALENT ways to do this...)
+        center_x = (float(col) + sigmoid(tx)) * 32.0
+        center_y = (float(row) + sigmoid(ty)) * 32.0
+
+        roi_w = np.exp(tw) * anchors[2*b + 0] * 32.0
+        roi_h = np.exp(th) * anchors[2*b + 1] * 32.0
+
+        final_confidence = sigmoid(tc)
+
+        # Find best class
+        class_predictions = predictions[row, col, b, 5:]
+        class_predictions = softmax(class_predictions)
+
+        class_predictions = tuple(class_predictions)
+        best_class = class_predictions.index(max(class_predictions))
+        best_class_score = class_predictions[best_class]
+
+        # Flip the coordinates on both axes
+        left   = int(center_x - (roi_w/2.))
+        right  = int(center_x + (roi_w/2.))
+        top    = int(center_y - (roi_h/2.))
+        bottom = int(center_y + (roi_h/2.))
+        
+        if( (final_confidence * best_class_score) > score_threshold):
+          thresholded_predictions.append([[left,top,right,bottom],final_confidence * best_class_score,classes[best_class]])
+
+  # Sort the B-boxes by their final score
+  thresholded_predictions.sort(key=lambda tup: tup[1],reverse=True)
+
+
+  # Non maximal suppression
+  #print('Non maximal suppression with iou threshold = {}'.format(iou_threshold))
+  nms_predictions = []
+  if(len(thresholded_predictions)>0):
+    nms_predictions = non_maximal_suppression(thresholded_predictions,iou_threshold)
+
+
+  # Draw final B-Boxes and label on input image
+  for i in range(len(nms_predictions)):
+      color = colors[classes.index(nms_predictions[i][2])]
+      best_class_name = nms_predictions[i][2]
+
+      # Put a class rectangle with B-Box coordinates and a class label on the image
+      input_image = cv2.rectangle(input_image,(nms_predictions[i][0][0],nms_predictions[i][0][1]),(nms_predictions[i][0][2],nms_predictions[i][0][3]),color)
+      cv2.putText(input_image,best_class_name,(int((nms_predictions[i][0][0]+nms_predictions[i][0][2])/2),int((nms_predictions[i][0][1]+nms_predictions[i][0][3])/2)),cv2.FONT_HERSHEY_SIMPLEX,1,color,3)
+  
+  return input_image
+
+
+
+
